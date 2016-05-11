ad7716ed6a166d0a17c168e275d2e15f5ffb6bce
Dava <david.martinez@unseen.is>
2016-05-11 18:00:09 -0400

    - updated UIView+shadow class

--- DMHorizontalView/UIView+Shadow.h
+++ DMHorizontalView/UIView+Shadow.h
@@ -1,8 +1,9 @@
 //
 //  UIView+Shadow.h
-//  Created by David Martinez Lebron on 5/12/15.
-//  Copyright (c) 2015 Davaur_David Martinez. All rights reserved.
+//  LogoPogo_Ver.2_iOS
 //
+//  Created by David Martinez Lebron on 5/19/15.
+//  Copyright (c) 2015 LogoPogo_David Martinez. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -14,6 +15,8 @@
  */
 -(void) addBottomShadow;
 
--(void) addShadow;
+-(void) addShadowWithColor:(UIColor *) shadowColor;
+
+-(void) removeShadow;
 
 @end
--- DMHorizontalView/UIView+Shadow.m
+++ DMHorizontalView/UIView+Shadow.m
@@ -1,9 +1,9 @@
 //
 //  UIView+Shadow.m
 //  LogoPogo_Ver.2_iOS
-//  Created by David Martinez Lebron on 5/12/15.
-//  Copyright (c) 2015 Davaur_David Martinez. All rights reserved.
 //
+//  Created by David Martinez Lebron on 5/19/15.
+//  Copyright (c) 2015 LogoPogo_David Martinez. All rights reserved.
 //
 
 #import "UIView+Shadow.h"
@@ -17,13 +17,20 @@
     self.layer.shadowColor = [UIColor darkGrayColor].CGColor;
 }
 
--(void) addShadow {
-    self.layer.shadowColor = [[UIColor blackColor] CGColor];
+-(void) addShadowWithColor:(UIColor *) shadowColor {
+    self.layer.shadowColor = [shadowColor CGColor];
     
-    self.layer.shadowOpacity = 0.8;
+    self.layer.shadowOpacity = 1.0;
     
     self.layer.shadowRadius = 2.0;
     
+    self.layer.shadowOffset = CGSizeMake(0, 0);
+}
+
+-(void) removeShadow {
+    self.layer.shadowColor = [[UIColor clearColor] CGColor];
+    self.layer.shadowOpacity = 0.0;
+    self.layer.shadowRadius = 0.0;
     self.layer.shadowOffset = CGSizeMake(0, 0);
 }
 
